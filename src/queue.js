6fff09d1f655a773654168c0f414c692aa9a6008
beaJenius <beajenius@yandex.ru>
2022-05-01 23:15:28 +0300

    feat: solve this problem

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  #head
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    //throw new NotImplementedError('Not implemented');
+    return this.#head
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    //throw new NotImplementedError('Not implemented');
+    let element = new ListNode(value)
+    
+    if (!this.#head) {
+      this.#head = element
+    } else {
+      let current = this.#head
+      while (current.next) {
+          current = current.next;
+      }    
+      current.next = element;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    //throw new NotImplementedError('Not implemented');
+    let headCopy = this.#head
+    this.#head = this.#head.next
+    return headCopy.value
   }
 }
 
